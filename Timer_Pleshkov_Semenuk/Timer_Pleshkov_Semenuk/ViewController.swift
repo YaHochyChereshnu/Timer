24a376661e800a32e2ced8e91407d8d6dff994e5
YaHochyChereshnu <75981571+YaHochyChereshnu@users.noreply.github.com>
2022-04-25 16:07:03 +0300

    Add files via upload

---
+++ Timer_Pleshkov_Semenuk/Timer_Pleshkov_Semenuk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Timer_Pleshkov_Semenuk
+//
+//  Created by Student on 25.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
